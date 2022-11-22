27df3ed25c90725a4a2f791a4e3808813b8c224d
Victor Harri-Chal <victor.harri-chal@epitech.eu>
2022-11-22 19:25:45 +0100

    [ADD] File testing

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 from protocol import *
 
 class Game:
@@ -8,6 +9,16 @@ class Game:
         self.protocol = Protocol()
         
     def run(self):
-        while (True):
-            self.protocol.readInput()
-            self.protocol.computeInput()
+        try:
+            if len(sys.argv) == 2:
+                file = open(sys.argv[1], "r")
+                while (True):
+                    self.protocol.readInput(sys.argv, file)
+                    self.protocol.computeInput(sys.argv, file)
+            elif len(sys.argv) == 1:
+                file = ""
+                while (True):
+                    self.protocol.readInput(sys.argv, file)
+                    self.protocol.computeInput(sys.argv, file)
+        except:
+            exit(0)
